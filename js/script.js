624ed4a210b7ace4153104e941db69fda4c78172
Simone Tarantino <questamailnonmiserveaniente@gmail.com>
2020-01-24 09:48:16 +0100

    Modifiche chat

--- js/script.js
+++ js/script.js
@@ -1,11 +1,24 @@
 $(document).ready (function(){
+// inviare messaggio con tasto enter
   $('#message').keypress(function (event) {
     if(event.which == 13 || event.keyCode == 13) {
-      var msg = $('#message').val();
-      console.log(msg);
-      var msgSect = $('.no-display .input-msg-container').clone();
-      msgSect.children().prepend(msg);
-      $('.main-chat').append(msgSect);
+      sendMsg();
     }
   });
+// inviare messaggio con click su icon
+  $('i.fa-paper-plane').click(function() {
+    sendMsg();
+  });
+
 });
+
+// funcion per inviare messaggio
+
+function sendMsg() {
+  var msg = $('#message').val();
+  console.log(msg);
+  var msgSect = $('.no-display .input-msg-container').clone();
+  msgSect.children().prepend(msg);
+  $('.main-chat').append(msgSect);
+  msg = $('#message').val('');
+}
