84d5bcf37e7d7fef8389d3dda08eb6010b9629c6
Simone Tarantino <questamailnonmiserveaniente@gmail.com>
2020-01-24 13:56:18 +0100

    Orario corrente x messaggio entrata e uscita

--- js/script.js
+++ js/script.js
@@ -20,22 +20,37 @@ $(document).ready (function(){
     $(this).parents('.go-delete').remove();
   });
 
-});
 
-// funcion per inviare messaggio
+  // funcion per inviare messaggio e ricevere risposta automatica
+  function sendMsg() {
+    var msg = $('#message').val();
+    if (msg.length >= 1) {
+      var msgSect = $('.no-display .input-msg-container').clone();
+      msgSect.children().prepend(msg);
+      $('.main-chat').append(msgSect);
+      msg = $('#message').val('');
 
-function sendMsg() {
-  var msg = $('#message').val();
-  if (msg.length >= 1) {
-    var msgSect = $('.no-display .input-msg-container').clone();
-    msgSect.children().prepend(msg);
-    $('.main-chat').append(msgSect);
-    msg = $('#message').val('');
-    setTimeout(function() {
-      var autoMsg = 'Ok';
-      var autoMsgSect = $('.no-display .auto-msg-container').clone();
-      autoMsgSect.children().prepend(autoMsg);
-      $('.main-chat').append(autoMsgSect);
-    }, 1000);
+      var time = new Date();
+      var hours = addZero(time.getHours());
+      var minutes = addZero(time.getMinutes());
+      var displayTime = hours +':'+ minutes;
+      msgSect.find('.msg-current-time').text(displayTime);
+
+      setTimeout(function() {
+        var autoMsg = 'Ok';
+        var autoMsgSect = $('.no-display .auto-msg-container').clone();
+        autoMsgSect.children().prepend(autoMsg);
+        $('.main-chat').append(autoMsgSect);
+        autoMsgSect.find('.msg-current-time').text(displayTime);
+      }, 1000);
+    }
   }
-}
+
+  function addZero(number) {
+    if(number < 10) {
+      number = '0' + number;
+    }
+    return number;
+  }
+
+});
